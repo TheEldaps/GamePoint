a69227a6531de7219bfda3488b3a798c82df8449
TheEldaps <adejames97@gmail.com>
2025-06-27 12:33:45 +0100

    refactor:add ful gamelist to the torunament section

--- src/Components/ContentTournament.jsx
+++ src/Components/ContentTournament.jsx
@@ -1,13 +1,16 @@
-import TournamentCard from "./TournamentCard";
+import GameList from "./GameList";
 
 export default function ContentTournament() {
   return (
-    <section className=" flex flex-col md:ml-[240px] w-[100vw] pt-[120px] ">
+    <div className=" flex flex-col md:ml-[240px] w-[100vw] pt-[120px] ">
       <div className="md:px-[100px] pb-[60px]">
         <h2 className=" font-[bold] pl-[40px] my-[0px] text-[2.5rem]">
           All Games
         </h2>
+        <section className="flex  flex-wrap  justify-evenly mt-[20px] gap-[40px] gap-y-[60px]">
+          <GameList />
+        </section>
       </div>
-    </section>
+    </div>
   );
 }
